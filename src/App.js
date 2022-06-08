1c6e73d7787d1c8707527ab1640d7559040d8a07
alexbellairs <alexanderbellairs@gmail.com>
2022-06-08 11:54:35 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import Numbers from "./components/Numbers";
+import Operators from "./components/Operators";
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [input, setInput] = useState("");
+  // function to calculate a result on a specific button click and update the state
+  const calculate = (button) => {
+    if (button === "=") {
+      const result = evaluate(input);
+      setInput(result);
+    } else if (button === "C") {
+      setInput("");
+    } else setInput(input + button);
+  };
+  const handleClick = (e) => {
+    const display = e.target.textContent;
+    calculate(display);
+  };
+  return (
+    <div className="App calculator">
+      <h1>My Calculator</h1>
+      <h2 className="calculator-screen">{input}</h2>
+      <Operators handleClick={handleClick} />
+      <Numbers handleClick={handleClick} />
+    </div>
+  );
+};
 
 export default App;
